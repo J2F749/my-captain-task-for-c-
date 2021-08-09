ffbcde219ec893ce1b52201590be5e341ed74447
J2F749 <84285194+J2F749@users.noreply.github.com>
2021-08-09 19:53:48 +0530

    Product of numbers
    Finding product of two different numbers

---
+++ product of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers:"<<a<<b;
+    cin>>a>>b;
+    c=a*b;
+    cout<<"product is:"<<c;
+    return 0;
+}
